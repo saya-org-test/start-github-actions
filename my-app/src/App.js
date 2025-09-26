9f5f62bec719839a02ecf1e52cc94eae598f5fcc
syjoy1993 <syjoy1993@gmail.com>
2025-09-26 09:53:53 +0900

    merge test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd1 sync test
+          Learn GithubAction cicd1 sync test ㅠㅠㅠ
         </a>
       </header>
     </div>
